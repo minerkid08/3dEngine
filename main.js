a4dff64866cb0d52255d9477e0772de27d0aeb72
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-05-11 14:17:17 -0400

    a

--- main.js
+++ main.js
@@ -105,8 +105,8 @@ class Engine{
     multiplyMatrix(i, m){
         let o = new vec3();
         o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-        o.x = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-        o.x = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
         let w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
 
         if(w != 0){
